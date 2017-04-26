08711a9dcd44103040d5b075383162dd417ec490
David <davidporter33@icloud.com>
2017-04-26 09:18:16 -0700

    added the book view controller

---
+++ CoreDataHotel/BookViewController.h
@@ -0,0 +1,13 @@
+//
+//  BookViewController.h
+//  CoreDataHotel
+//
+//  Created by David Porter on 4/26/17.
+//  Copyright © 2017 David Porter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BookViewController : UIViewController
+
+@end
